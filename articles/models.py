4fd5f29cceb2b6564cb70f1877d7ac9aca729304
Jordan-LE <80821726+Jordan-LE@users.noreply.github.com>
2021-08-06 22:00:26 +0200

    Création des modèles Article et Tag

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+        
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    image = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
