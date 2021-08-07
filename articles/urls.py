c3a2ace8d001624c1a8ba9a2a03eae2f5c93de8f
Jordan-LE <80821726+Jordan-LE@users.noreply.github.com>
2021-08-07 14:19:56 +0200

    Ajout page détail d'un article

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /
     path('', views.index, name='index'),
+
+    # /{article_id}
+    path('<int:article_id>/', views.detail, name='detail')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Article
 
@@ -6,4 +6,10 @@ def index(request):
     articles = Article.objects.all()
     return render(request, 'articles/index.html', {
         'articles': articles
+    })
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk = article_id)
+    return render(request, 'articles/detail.html', {
+        'article': article
     })
